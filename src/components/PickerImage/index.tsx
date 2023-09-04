9f3cbe90a4d9c98855270cc2a1343d31d4d85354
Michel John <michel.john@hotmail.com>
2023-09-04 14:56:13 -0300

    implements ocr

--- src/components/PickerImage/index.tsx
+++ src/components/PickerImage/index.tsx
@@ -3,10 +3,24 @@ import {
   launchImageLibrary,
   ImagePickerResponse,
 } from 'react-native-image-picker';
-import {useState} from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import { useCallback, useState } from 'react';
+import Icon from 'react-native-vector-icons/Entypo';
+import TextRecognition from '@react-native-ml-kit/text-recognition';
 
-import {Button} from '../Button';
+import { Button } from '../Button';
+
+interface OcrProps {
+  text: string;
+  isLoading: boolean;
+}
 
 interface Toast {
   message: string;
@@ -21,6 +35,10 @@ interface StateProps {
 }
 
 export function PickerImage() {
+  const [ocr, setOrc] = useState<OcrProps>({
+    text: '',
+    isLoading: false,
+  });
   const [state, setState] = useState<StateProps>({
     image: null,
     loading: false,
@@ -31,18 +49,32 @@ export function PickerImage() {
     },
   });
 
+  const onOCR = useCallback(
+    async (image: string) => {
+      setOrc({ ...ocr, isLoading: true });
+
+      const result = await TextRecognition.recognize(image);
+
+      setOrc({ isLoading: false, text: result.text });
+    },
+    [ocr],
+  );
+
   function onImageSelect(response: ImagePickerResponse) {
     if (response.assets && response.assets[0].uri) {
-      setState({...state, image: response.assets[0].uri});
+      const uri = response.assets[0].uri;
+
+      setState({ ...state, loading: false, image: uri });
+      onOCR(uri);
     }
   }
 
   function handleGetImage(type: 'capture' | 'library') {
-    setState({...state, loading: true});
+    setState({ ...state, loading: true });
 
     type === 'capture'
-      ? launchCamera({mediaType: 'photo'}, onImageSelect)
-      : launchImageLibrary({mediaType: 'photo'}, onImageSelect);
+      ? launchCamera({ mediaType: 'photo' }, onImageSelect)
+      : launchImageLibrary({ mediaType: 'photo' }, onImageSelect);
   }
 
   return (
@@ -54,13 +86,35 @@ export function PickerImage() {
 
       <Button title="Tirar foto" onPress={() => handleGetImage('capture')} />
 
+      {state.loading && (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#ffc719" />
+        </View>
+      )}
+
       {state.image && (
-        <Image
-          style={styles.image}
-          source={{
-            uri: state.image,
-          }}
-        />
+        <View style={styles.wrapperImage}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: state.image,
+            }}
+          />
+
+          <Icon name="chevron-down" size={30} color="#ffc719" />
+        </View>
+      )}
+
+      {ocr.isLoading && (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#ffc719" />
+        </View>
+      )}
+
+      {ocr.text && (
+        <ScrollView style={styles.ocrView}>
+          <Text style={styles.text}>{ocr.text}</Text>
+        </ScrollView>
       )}
     </View>
   );
@@ -69,10 +123,34 @@ export function PickerImage() {
 const styles = StyleSheet.create({
   container: {
     gap: 8,
+    width: '100%',
+  },
+  loading: {
+    height: 400,
+    marginTop: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapperImage: {
+    width: 400,
+    height: 310,
+    marginTop: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
-    width: 400,
-    height: 400,
-    marginTop: 32,
+    width: '100%',
+    height: '100%',
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  ocrView: {
+    height: 300,
+  },
+  text: {
+    color: '#ffc719',
+    fontSize: 18,
+    marginVertical: 16,
   },
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,10 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
-import {PickerImage} from '../../components/PickerImage';
+import { PickerImage } from '../../components/PickerImage';
 
 export function Home() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>
-        Selecione uma imagem da galeria ou tire uma foto para fazer a extração
-        do texto
-      </Text>
-
       <PickerImage />
     </SafeAreaView>
   );
@@ -20,12 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 32,
     alignItems: 'center',
-    paddingHorizontal: 16,
+    paddingHorizontal: 24,
     backgroundColor: '#010001',
-  },
-  text: {
-    color: '#ffc719',
-    fontSize: 18,
-    marginVertical: 16,
   },
 });
